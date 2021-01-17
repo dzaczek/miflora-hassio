58aa62fae461ec1443a1acf41f3834592e220167
dzaczek <sirjacek@gmail.com>
2021-01-17 08:46:27 +0000

    Create btMQTTgatewayflash.sh

---
+++ btMQTTgatewayflash.sh
@@ -0,0 +1,1 @@
+esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio  --flash_freq 40m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_dio_80m.bin 0x10000 esp32dev-ble-firmware.bin  0x8000 esp32dev-ble-partitions.bin 
